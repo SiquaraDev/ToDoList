3c5bb6125b238098b16ca2c852f8d73c44b1c79c
SiquaraDev <matheus.siquara99@gmail.com>
2024-04-26 14:45:11 -0300

    Update: initial config

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import {SafeAreaView,Text} from 'react-native';
+
+import {styles} from './styles/styleApp';
+
+
+function App() {
+  
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
