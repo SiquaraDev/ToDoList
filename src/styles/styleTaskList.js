14877f805f0deab29dfeacfcdd04b00c45ad765c
SiquaraDev <matheus.siquara99@gmail.com>
2024-04-26 15:16:16 -0300

    Update: new TaskList and organizing common styles

--- src/styles/styleTaskList.js
+++ src/styles/styleTaskList.js
@@ -1,10 +1,31 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from 'react-native';
+
+import cs from './commonStyles';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
-})
+  background: {
+    flex: 3,
+  },
+  TaskList: {
+    flex: 7,
+  },
+  titleBar: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    padding: 20,
+  },
+  title: {
+    fontFamily: cs.fontFamily,
+    fontSize: 50,
+    color: cs.colors.secondary,
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontFamily: cs.fontFamily,
+    fontSize: 20,
+    color: cs.colors.secondary,
+  },
+});
--- src/views/TaskList.jsx
+++ src/views/TaskList.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, View, Text, ImageBackground} from 'react-native';
 
 import {styles} from '../styles/styleTaskList';
 
+import todayImage from '../assets/images/today.jpg';
+
+import moment from 'moment';
+
 export default props => {
+  const today = moment().format('dddd, MMM Do');
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World</Text>
+      <ImageBackground source={todayImage} style={styles.background}>
+        <View style={styles.titleBar}>
+          <Text style={styles.title}>Today</Text>
+          <Text style={styles.subtitle}>{today}</Text>
+        </View>
+      </ImageBackground>
+      <View style={styles.TaskList}>
+        <Text>#Task 1</Text>
+        <Text>#Task 2</Text>
+        <Text>#Task 3</Text>
+      </View>
     </SafeAreaView>
   );
 };
