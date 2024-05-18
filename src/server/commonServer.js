0ac4d5ff25bd8cd3e20cf34089c076c75afd66f9
SiquaraDev <matheus.siquara99@gmail.com>
2024-05-17 23:30:48 -0300

    Update: changing for better error messages

--- src/server/commonServer.js
+++ src/server/commonServer.js
@@ -4,7 +4,11 @@ const server =
   Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000';
 
 const showError = err => {
-  Alert.alert('Something wrong.', `Message: ${err}`);
+  if (err.response && err.response.data) {
+    Alert.alert('Something wrong.', `Message: ${err.response.data}`);
+  } else {
+    Alert.alert('Something wrong.', `Message: ${err}`);
+  }
 };
 
 const showSuccess = msg => {
